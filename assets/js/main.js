de01c35878ab1fb6602ef66df898e241ae9353d0
Ubuntu <ubuntu@ip-172-31-34-87.ec2.internal>
2019-12-31 10:12:11 +0000

    JS added and more HTML tweaks on slideshow

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slideshow");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("slide");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {

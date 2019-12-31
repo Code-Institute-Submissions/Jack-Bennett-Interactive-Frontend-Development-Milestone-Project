d70efd37ab790555acba6375dee2bcca882e86d5
Ubuntu <ubuntu@ip-172-31-34-87.ec2.internal>
2019-12-31 10:16:58 +0000

    Countdown timer added using JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// Slideshow - Code assisted by W3 Schools
+
 function currentDiv(n) {
   showDivs(slideIndex = n);
 }
@@ -17,3 +19,27 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " slide-opacity-off";
 }
+
+// Countdown Clock - Code assisted by W3 Schools
+
+var countDownDate = new Date("July 15, 2020 23:59:59").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+    
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("countdown-clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Offer has now finished!";
+  }
+}, 1000);

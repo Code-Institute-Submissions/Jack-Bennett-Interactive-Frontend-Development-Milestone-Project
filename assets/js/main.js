1e50356f74c5fffaaa1b58b3b47f94bd00c93d8c
Ubuntu <ubuntu@ip-172-31-34-87.ec2.internal>
2020-01-21 15:17:03 +0000

    JavaScript added for holiday search bar + minor tweaks to search bar HTML and CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,25 @@ var x = setInterval(function() {
     document.getElementById("countdown-timer").innerHTML = "Offer has now finished!";
   }
 }, 1000);
+
+// Search Bar - Code assisted by W3 Schools
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}

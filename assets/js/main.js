017ecd6bb5d65d7d729157cb0faf736316701208
Ubuntu <ubuntu@ip-172-31-34-87.ec2.internal>
2020-01-21 12:27:16 +0000

    Links added behind travel package images + excess JavaScript code removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,15 +43,3 @@ var x = setInterval(function() {
     document.getElementById("countdown-timer").innerHTML = "Offer has now finished!";
   }
 }, 1000);
-
-// Countdown Clock - Code assisted by 'Google Maps' lessons
-
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: {
-      lat: 46.619261,
-      lng: -33.134766
-    }
-  });
-}
